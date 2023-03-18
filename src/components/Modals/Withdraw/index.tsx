2471eaf02128b32121dc59f048666fa74912a4d9
Sávio Pimenta <saviopimentaalexandrino33@gmail.com>
2023-03-17 21:38:53 -0300

    Withdraw finish implementation

--- src/components/Modals/Withdraw/index.tsx
+++ src/components/Modals/Withdraw/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react'
-import { Text, TextInput, View } from 'react-native'
+import { Keyboard, Text, TextInput, TouchableWithoutFeedback, View } from 'react-native'
 
 import * as S from './styles'
 import { useUser } from '../../../contexts/userContext'
@@ -49,7 +49,9 @@ const Withdraw: React.FC<WithdrawProps> = ({
   const handleWithdraw = async () => {
     setIsLoading(true)
     try {
-      await withDraw(parseFloat(value))
+      const newValue = value.replaceAll('.', '')
+
+      await withDraw(parseFloat(newValue))
       await handleGetBalance()
       setShowWithdraw(!showWithdraw)
 
@@ -66,111 +68,114 @@ const Withdraw: React.FC<WithdrawProps> = ({
   }, [])
 
   return (
-    <S.Container>
-      <S.ContentModal>
-        <S.Header>
-          <S.HeaderTitle>Withdraw</S.HeaderTitle>
-          <S.CloseButton onPress={() => setShowWithdraw(!showWithdraw)}>
-            <Ionicons name="ios-close" size={24} color={colors.dark2} />
-          </S.CloseButton>
-        </S.Header>
-        <S.Body>
-          <S.ValueContainer>
-            <S.ValuePrefix>€</S.ValuePrefix>
-            <View style={{ width: 8 }} />
-            <View
-              style={{
-                position: 'relative',
-                alignItems: 'center',
-                justifyContent: 'center',
-              }}
-            >
-              <Text
+    <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+      <S.Container>
+        <S.ContentModal>
+          <S.Header>
+            <S.HeaderTitle>Withdraw</S.HeaderTitle>
+            <S.CloseButton onPress={() => setShowWithdraw(!showWithdraw)}>
+              <Ionicons name="ios-close" size={24} color={colors.dark2} />
+            </S.CloseButton>
+          </S.Header>
+          <S.Body>
+            <S.ValueContainer>
+              <S.ValuePrefix>€</S.ValuePrefix>
+              <View style={{ width: 8 }} />
+              <View
                 style={{
-                  fontFamily: 'Poppins_400Regular',
-                  fontSize: 40,
-                  width: 'auto',
+                  position: 'relative',
+                  alignItems: 'center',
+                  justifyContent: 'center',
                 }}
-                numberOfLines={1}
-                onLayout={(e) => {
-                  const { width } = e.nativeEvent.layout
-                  inputRef.current?.setNativeProps({
-                    style: {
-                      width,
-                    },
-                  })
-                }}
-                ref={invisibleText}
               >
-                {value}
-              </Text>
-              <S.ValueInput
-                ref={inputRef}
-                keyboardType="number-pad"
-                defaultValue={value}
-                maxLength={10}
-                onChange={(e) => {
-                  const newValue = moneyMask(e.nativeEvent.text)
-                  if (newValue.length <= 10) {
-                    setValue(newValue)
-                  }
-                }}
-              />
-            </View>
-          </S.ValueContainer>
-          <View style={{ height: 8 }} />
-          <S.ValueSubtitle>Set the value for you withdraw</S.ValueSubtitle>
-          <View style={{ height: 8 }} />
-          <S.Divisor />
-          <View style={{ height: 8 }} />
-          <S.Title>From</S.Title>
-          <View style={{ height: 8 }} />
-          <S.Card>
-            <S.MoovinLogo>
-              <Logo />
-            </S.MoovinLogo>
-            <View style={{ width: 16 }} />
-            <S.CardContent>
-              <S.CardTitle>Moovin wallet</S.CardTitle>
-              <View style={{ height: 2 }} />
-              <S.CardText>
-                <S.CardBlackText>
-                  {currencyFormatter.format(balance)}
-                </S.CardBlackText>{' '}
-                available
-              </S.CardText>
-            </S.CardContent>
-          </S.Card>
-          <View style={{ height: 8 }} />
-          <S.Title>To</S.Title>
-          <View style={{ height: 8 }} />
-          <S.Card>
-            <S.MoovinLogo>
-              <MaterialCommunityIcons
-                name="bank-outline"
-                size={24}
-                color={colors.dark2}
-              />
-            </S.MoovinLogo>
-            <View style={{ width: 16 }} />
-            <S.CardContent>
-              <S.CardTitle>{user?.bankName}</S.CardTitle>
-              <View style={{ height: 2 }} />
-              <S.CardText>
-                IE****************{user?.bankDigits?.slice(-4)}
-              </S.CardText>
-            </S.CardContent>
-          </S.Card>
-          <View style={{ height: 8 }} />
-          <S.Button
-            disabled={balance < parseFloat(value)}
-            onPress={handleWithdraw}
-          >
-            <S.ButtonText>Transfer</S.ButtonText>
-          </S.Button>
-        </S.Body>
-      </S.ContentModal>
-    </S.Container>
+                <Text
+                  style={{
+                    fontFamily: 'Poppins_400Regular',
+                    fontSize: 40,
+                    width: 'auto',
+                  }}
+                  numberOfLines={1}
+                  onLayout={(e) => {
+                    const { width } = e.nativeEvent.layout
+                    inputRef.current?.setNativeProps({
+                      style: {
+                        width,
+                      },
+                    })
+                  }}
+                  ref={invisibleText}
+                >
+                  {value}
+                </Text>
+                <S.ValueInput
+                  ref={inputRef}
+                  keyboardType="number-pad"
+                  returnKeyType="done"
+                  defaultValue={value}
+                  maxLength={10}
+                  onChange={(e) => {
+                    const newValue = moneyMask(e.nativeEvent.text)
+                    if (newValue.length <= 10) {
+                      setValue(newValue)
+                    }
+                  }}
+                />
+              </View>
+            </S.ValueContainer>
+            <View style={{ height: 8 }} />
+            <S.ValueSubtitle>Set the value for you withdraw</S.ValueSubtitle>
+            <View style={{ height: 8 }} />
+            <S.Divisor />
+            <View style={{ height: 8 }} />
+            <S.Title>From</S.Title>
+            <View style={{ height: 8 }} />
+            <S.Card>
+              <S.MoovinLogo>
+                <Logo />
+              </S.MoovinLogo>
+              <View style={{ width: 16 }} />
+              <S.CardContent>
+                <S.CardTitle>Moovin wallet</S.CardTitle>
+                <View style={{ height: 2 }} />
+                <S.CardText>
+                  <S.CardBlackText>
+                    {currencyFormatter.format(balance)}
+                  </S.CardBlackText>{' '}
+                  available
+                </S.CardText>
+              </S.CardContent>
+            </S.Card>
+            <View style={{ height: 8 }} />
+            <S.Title>To</S.Title>
+            <View style={{ height: 8 }} />
+            <S.Card>
+              <S.MoovinLogo>
+                <MaterialCommunityIcons
+                  name="bank-outline"
+                  size={24}
+                  color={colors.dark2}
+                />
+              </S.MoovinLogo>
+              <View style={{ width: 16 }} />
+              <S.CardContent>
+                <S.CardTitle>{user?.bankName}</S.CardTitle>
+                <View style={{ height: 2 }} />
+                <S.CardText>
+                  IE****************{user?.bankDigits?.slice(-4)}
+                </S.CardText>
+              </S.CardContent>
+            </S.Card>
+            <View style={{ height: 8 }} />
+            <S.Button
+              disabled={balance < parseFloat(value)}
+              onPress={handleWithdraw}
+            >
+              <S.ButtonText>Transfer</S.ButtonText>
+            </S.Button>
+          </S.Body>
+        </S.ContentModal>
+      </S.Container>
+    </TouchableWithoutFeedback>
   )
 }
 
